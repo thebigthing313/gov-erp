969d31aa3bf99a31329a84d1133cd13795cd955b
thebigthing313 <adriankabigting@gmail.com>
2025-10-10 16:37:46 -0400

    fix: standardize quotation marks in company data

--- src/data/company-data.ts
+++ src/data/company-data.ts
@@ -1,7 +1,9 @@
 export const company = {
-  name: 'Middlesex County Mosquito Extermination Commission',
-  address: '200 Parsonage Road, Edison, NJ 08837',
-  phone: '7325490665',
-  fax: '7326030820',
-  acronym: 'MCMEC',
-}
+  name: "Middlesex County Mosquito Extermination Commission",
+  address: "200 Parsonage Road, Edison, NJ 08837",
+  phone: "7325490665",
+  fax: "7326030820",
+  acronym: "MCMEC",
+};
+
+export type Company = typeof company;
