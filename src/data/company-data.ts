84648dd5a8f0070b943d2baa07e63838bfa22d79
thebigthing313 <adriankabigting@gmail.com>
2025-10-10 15:29:56 -0400

    feat: add company data with contact information

---
+++ src/data/company-data.ts
@@ -0,0 +1,7 @@
+export const company = {
+  name: 'Middlesex County Mosquito Extermination Commission',
+  address: '200 Parsonage Road, Edison, NJ 08837',
+  phone: '7325490665',
+  fax: '7326030820',
+  acronym: 'MCMEC',
+}
