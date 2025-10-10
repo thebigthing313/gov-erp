de4d090f1e3834cd32e884e9dfb5eb7498076b72
thebigthing313 <adriankabigting@gmail.com>
2025-10-10 14:51:09 -0400

    feat: reusable textinput component

---
+++ src/components/form-fields/text-input.tsx
@@ -0,0 +1,37 @@
+import { Field, FieldDescription, FieldError, FieldLabel } from '../ui/field'
+import { InputGroup, InputGroupAddon, InputGroupInput } from '../ui/input-group'
+import { Spinner } from '../ui/spinner'
+
+interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  description?: string
+  isLoading?: boolean
+  errors?: ({ message?: string } | undefined)[]
+  label?: string
+}
+
+export function TextInput({
+  label,
+  description,
+  isLoading,
+  errors,
+  ...props
+}: TextInputProps) {
+  return (
+    <Field data-invalid={!!errors?.length}>
+      <FieldLabel>{label}</FieldLabel>
+      <InputGroup>
+        <InputGroupInput
+          {...props}
+          aria-invalid={!!errors?.length}
+        ></InputGroupInput>
+        {isLoading && (
+          <InputGroupAddon align="inline-end">
+            <Spinner />
+          </InputGroupAddon>
+        )}
+      </InputGroup>
+      <FieldDescription>{description}</FieldDescription>
+      <FieldError errors={errors} />
+    </Field>
+  )
+}
