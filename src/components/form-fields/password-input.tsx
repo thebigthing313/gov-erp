602275d7fed6a90ab03a051af0220b169a0b390d
thebigthing313 <adriankabigting@gmail.com>
2025-10-10 14:51:24 -0400

    feat: reusable passwordinput component

---
+++ src/components/form-fields/password-input.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { Field, FieldDescription, FieldError, FieldLabel } from '../ui/field'
+import { InputGroup, InputGroupAddon, InputGroupInput } from '../ui/input-group'
+import { Button } from '../ui/button'
+import { EyeIcon, EyeOff } from 'lucide-react'
+
+interface PasswordInputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> {
+  description?: string
+  isLoading?: boolean
+  errors?: ({ message?: string } | undefined)[]
+  label?: string
+}
+
+export function PasswordInput({
+  label,
+  description,
+  errors,
+  ...props
+}: PasswordInputProps) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+  return (
+    <Field data-invalid={!!errors?.length}>
+      <FieldLabel>{label}</FieldLabel>
+      <InputGroup>
+        <InputGroupInput
+          type={isPasswordVisible ? 'text' : 'password'}
+          {...props}
+          aria-invalid={!!errors?.length}
+        ></InputGroupInput>
+
+        <InputGroupAddon align="inline-end">
+          <Button
+            variant="ghost"
+            onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+          >
+            {isPasswordVisible ? <EyeOff /> : <EyeIcon />}
+          </Button>
+        </InputGroupAddon>
+      </InputGroup>
+      <FieldDescription>{description}</FieldDescription>
+      <FieldError errors={errors} />
+    </Field>
+  )
+}
