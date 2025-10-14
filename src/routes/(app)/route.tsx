20ee855513cd6403ea4f6c2e2f9f4172dc25476f
thebigthing313 <adriankabigting@gmail.com>
2025-10-14 16:04:20 -0400

    refactor: simplify authentication logic in app route and enhance layout styling

--- src/routes/(app)/route.tsx
+++ src/routes/(app)/route.tsx
@@ -1,20 +1,11 @@
-import { getPermissions, getUserId, sessionExists } from '@/lib/auth'
+import { getAuth, isAuthenticated, Auth } from '@/lib/auth'
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(app)')({
   beforeLoad: async ({ context }) => {
-    const activeSession = await sessionExists(context.supabase)
-    if (!activeSession) throw redirect({ to: '/login' })
-    const user_id = await getUserId(context.supabase)
-    if (!user_id) throw redirect({ to: '/login' })
-    const profile_id = await getUserId(context.supabase)
-    if (!profile_id)
-      throw new Error(
-        'Your account was set up incorrectly. User has no linked profile.',
-      )
-    const permissions = await getPermissions(context.supabase)
-
-    return { user_id, profile_id, permissions }
+    const auth = await getAuth(context.supabase)
+    if (isAuthenticated(auth) === false) redirect({ to: '/login' })
+    return { auth: auth as Auth }
   },
   component: RouteComponent,
   loader: async () => {},
@@ -22,7 +13,7 @@ export const Route = createFileRoute('/(app)')({
 
 function RouteComponent() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center p-6">
+    <div className="w-screen h-screen max-h-screen max-w-screen overflow-hidden flex items-center justify-center p-6">
       <Outlet />
     </div>
   )
