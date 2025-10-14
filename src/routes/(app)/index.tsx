7cf22264770ce6ab0098fc04442848da0318a4ee
thebigthing313 <adriankabigting@gmail.com>
2025-10-14 16:04:31 -0400

    feat: integrate PendingComponent and enhance app list rendering in RouteComponent

--- src/routes/(app)/index.tsx
+++ src/routes/(app)/index.tsx
@@ -7,27 +7,32 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import { Skeleton } from '@/components/ui/skeleton'
 import { employeeInfoQueryOptions } from '@/queries/employees/query-options'
 import { useSuspenseQuery } from '@tanstack/react-query'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { AppCard } from '@/components/cards/app-card'
+import { appList } from '@/data/company-app-list'
+import { PendingComponent } from '@/components/pending-component'
 
 export const Route = createFileRoute('/(app)/')({
-  component: RouteComponent,
   pendingComponent: PendingComponent,
+  component: RouteComponent,
   loader: async ({ context }) => {
     context.queryClient.ensureQueryData(
-      employeeInfoQueryOptions(context.supabase, context.user_id),
+      employeeInfoQueryOptions(context.supabase, context.auth.userId),
     )
   },
 })
 
 function RouteComponent() {
-  const { supabase, user_id, company } = Route.useRouteContext()
-  const { data } = useSuspenseQuery(employeeInfoQueryOptions(supabase, user_id))
+  const { supabase, company, auth } = Route.useRouteContext()
+  const { data } = useSuspenseQuery(
+    employeeInfoQueryOptions(supabase, auth.userId),
+  )
+  const navigate = useNavigate()
 
   return (
-    <Card className="min-w-sm max-w-xl w-full">
+    <Card className="min-w-sm max-w-xl not-visited:w-full max-h-full flex flex-col">
       <CardHeader>
         <CardTitle>
           <Typography tag="h2">Welcome, {data.first_name}!</Typography>
@@ -39,28 +44,32 @@ function RouteComponent() {
           </Typography>
         </CardDescription>
       </CardHeader>
-      <CardContent>list apps here</CardContent>
+      <CardContent className="overflow-y-auto flex-1">
+        {appList.map((app) => {
+          if (
+            !app.requiredPermission ||
+            auth.permissions.includes(app.requiredPermission)
+          ) {
+            return (
+              <AppCard
+                variant="muted"
+                key={app.name}
+                title={app.name}
+                description={app.description}
+                icon={app.icon}
+                version={app.version}
+                changelogLink={app.changelogUrl}
+                onOpen={() => navigate(app.appUrl)}
+              />
+            )
+          }
+        })}
+      </CardContent>
       <CardFooter className="grid text-xs text-muted-foreground tracking-tight">
         <div>{company.name}</div>
         <div>{company.address}</div>
         <div>Phone: {company.phone}</div>
         <div>Fax: {company.fax}</div>
-      </CardFooter>
-    </Card>
-  )
-}
-
-function PendingComponent() {
-  return (
-    <Card className="min-w-md max-w-lg mx-auto">
-      <CardHeader>
-        <CardTitle>
-          <Skeleton />
-        </CardTitle>
-      </CardHeader>
-      <CardFooter className="grid gap-0">
-        <Skeleton />
-        <Skeleton />
       </CardFooter>
     </Card>
   )
