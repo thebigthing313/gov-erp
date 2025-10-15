6e9fbc09b8a33622bd0e61ba206221e8979f37a5
thebigthing313 <adriankabigting@gmail.com>
2025-10-15 16:29:51 -0400

    feat: add EmployeePortalSidebarContent component with dashboard and profile links

---
+++ src/routes/employee-portal/-components/sidebar-content.tsx
@@ -0,0 +1,47 @@
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import { Link, LinkProps } from '@tanstack/react-router'
+import { LayoutDashboard, User } from 'lucide-react'
+
+type SidebarItem = {
+  icon: React.ReactNode
+  label: string
+  linkProps: LinkProps
+}
+const sidebarItems: Array<SidebarItem> = [
+  {
+    icon: <LayoutDashboard />,
+    label: 'Dashboard',
+    linkProps: { to: '/employee-portal' },
+  },
+  {
+    icon: <User />,
+    label: 'Profile',
+    linkProps: { to: '/employee-portal/profile' },
+  },
+]
+export function EmployeePortalSidebarContent() {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {sidebarItems.map((item) => (
+            <SidebarMenuItem key={`sidebar-menu-item-${item.label}`}>
+              <Link {...item.linkProps}>
+                <SidebarMenuButton tooltip={item.label}>
+                  {item.icon}
+                  <span>{item.label}</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
