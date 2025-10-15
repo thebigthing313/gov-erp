2a6088cfad12621092d85cfe9a8928eeae559554
thebigthing313 <adriankabigting@gmail.com>
2025-10-15 14:39:37 -0400

    feat: enhance employee portal route with authentication checks and sidebar layout

--- src/routes/employee-portal/route.tsx
+++ src/routes/employee-portal/route.tsx
@@ -1,9 +1,52 @@
-import { createFileRoute } from '@tanstack/react-router'
+import {
+  SharedLayoutHeader,
+  SharedLayoutOutlet,
+  SharedLayoutSidebar,
+} from '@/components/layout/shared-layout'
+import { Typography } from '@/components/typography'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { Auth, getAuth, isAuthenticated } from '@/lib/auth'
+import {
+  employeeInfoQueryOptions,
+  employeeTitlesByUserIdQueryOptions,
+} from '@/queries/employees/query-options'
+
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/employee-portal')({
+  beforeLoad: async () => {
+    const auth = await getAuth()
+
+    if (!isAuthenticated(auth)) {
+      throw redirect({ to: '/login' })
+    }
+
+    return { auth: auth as Auth }
+  },
   component: RouteComponent,
+  loader: async ({ context }) => {
+    const { queryClient, auth } = context
+    queryClient.ensureQueryData(employeeInfoQueryOptions(auth.userId))
+    queryClient.ensureQueryData(employeeTitlesByUserIdQueryOptions(auth.userId))
+  },
 })
 
 function RouteComponent() {
-  return <div>Hello "/employee-portal"!</div>
+  const { auth } = Route.useRouteContext()
+
+  return (
+    <SidebarProvider>
+      <SharedLayoutSidebar user_id={auth.userId}>
+        SIDEBARCONTENT
+      </SharedLayoutSidebar>
+      <SidebarInset>
+        <SharedLayoutHeader>
+          <Typography tag="h4">Employee Portal</Typography>
+        </SharedLayoutHeader>
+        <SharedLayoutOutlet>
+          <Outlet />
+        </SharedLayoutOutlet>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
