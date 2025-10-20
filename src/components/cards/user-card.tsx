74c3dc0367c154ccee233924fdd6552ad9bd05b6
thebigthing313 <adriankabigting@gmail.com>
2025-10-20 14:56:13 -0400

    feat: refactor UserCard component to use employee data for rendering

--- src/components/cards/user-card.tsx
+++ src/components/cards/user-card.tsx
@@ -6,25 +6,37 @@ import {
   ItemTitle,
 } from '../ui/item'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { useEmployee } from '@/db/hooks/use-employee'
+import { useEmployeeTitles } from '@/db/hooks/use-employee-titles'
 
 interface UserCardProps extends React.ComponentProps<typeof Item> {
-  name: string
-  title?: string
+  employee_id: string
   avatarSrc?: string
 }
 
-export function UserCard({ name, avatarSrc, title, ...props }: UserCardProps) {
+export function UserCard({ employee_id, ...props }: UserCardProps) {
+  const { query: employeeQuery } = useEmployee(employee_id)
+  const { query: employeeTitlesQuery } = useEmployeeTitles(employee_id)
+
+  const employee = employeeQuery.data[0]
+  const currentTitle = employeeTitlesQuery.data[0]
+
   return (
     <Item {...props}>
       <ItemMedia variant="image" className="size-12">
         <Avatar>
-          <AvatarImage src={avatarSrc} alt={title} />
-          <AvatarFallback>{name?.charAt(0)}</AvatarFallback>
+          <AvatarImage
+            src={employee.photo_url ?? undefined}
+            alt={employee.first_name}
+          />
+          <AvatarFallback>{employee?.first_name.charAt(0)}</AvatarFallback>
         </Avatar>
       </ItemMedia>
       <ItemContent>
-        <ItemTitle>{name}</ItemTitle>
-        <ItemDescription>{title}</ItemDescription>
+        <ItemTitle>
+          {employee.first_name} {employee.last_name}
+        </ItemTitle>
+        <ItemDescription>{currentTitle.titles.title_name}</ItemDescription>
       </ItemContent>
     </Item>
   )
