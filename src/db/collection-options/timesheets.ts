a16807279b311ddf8e082b42296d992b9b74909f
thebigthing313 <adriankabigting@gmail.com>
2025-10-20 09:58:14 -0400

    feat: add TimesheetsByYear collection options for querying timesheet data

---
+++ src/db/collection-options/timesheets.ts
@@ -0,0 +1,52 @@
+import * as TanstackQueryProvider from "@/integrations/tanstack-query/root-provider";
+import { Table } from "@/lib/data-types";
+import { supabase } from "@/main";
+import { queryCollectionOptions } from "@tanstack/query-db-collection";
+import { dbDelete, dbInsert, dbUpdate } from "../db-functions";
+
+const { queryClient } = TanstackQueryProvider.getContext();
+
+const table: Table = "timesheets";
+
+export const TimesheetsByYearCollectionOptions = (year: number) =>
+    queryCollectionOptions({
+        queryKey: [table, "year", year],
+        queryFn: async () => {
+            const { data, error } = await supabase.from(table).select(
+                "*, pay_periods(payroll_year)",
+            ).eq(
+                "pay_periods.payroll_year",
+                year,
+            );
+            if (error) throw error;
+            return data;
+        },
+        queryClient,
+        getKey: (item) => item.id,
+        onInsert: async ({ transaction, collection }) => {
+            const localNewItems = transaction.mutations.map((m) => m.modified);
+            const serverNewItems = await dbInsert(table, localNewItems);
+
+            serverNewItems.forEach((item) =>
+                collection.utils.writeUpsert(item)
+            );
+            return { refetch: false };
+        },
+        onUpdate: async ({ transaction, collection }) => {
+            const localUpdatedItems = transaction.mutations.map((m) => ({
+                id: m.key,
+                changes: m.changes,
+            }));
+            const serverUpdatedItems = await dbUpdate(table, localUpdatedItems);
+
+            serverUpdatedItems.forEach((item) => {
+                collection.utils.writeUpsert(item);
+            });
+            return { refetch: false };
+        },
+        onDelete: async ({ transaction }) => {
+            const localDeletedItemIds = transaction.mutations.map((m) => m.key);
+            await dbDelete(table, localDeletedItemIds);
+            return { refetch: false };
+        },
+    });
