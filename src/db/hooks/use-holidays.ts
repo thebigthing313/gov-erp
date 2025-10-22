41e54b68525e616057e712545ff40ec5f19a84d1
thebigthing313 <adriankabigting@gmail.com>
2025-10-22 16:24:24 -0400

    feat: refactor useHolidays hook to improve query structure and return values

--- src/db/hooks/use-holidays.ts
+++ src/db/hooks/use-holidays.ts
@@ -1,11 +1,18 @@
-import { and, eq, gte, lte, useLiveQuery } from "@tanstack/react-db";
+import {
+    and,
+    eq,
+    gte,
+    isUndefined,
+    lte,
+    useLiveQuery,
+} from "@tanstack/react-db";
 import { holidayDatesCollection, holidaysCollection } from "../collections";
 
 export function useHolidays(year: number) {
     const startOfYear = new Date(`${year}-01-01`);
     const endOfYear = new Date(`${year}-12-31`);
 
-    const query = useLiveQuery(
+    const holidayDatesByYear = useLiveQuery(
         (q) =>
             q.from({ holiday_dates: holidayDatesCollection }).innerJoin(
                 { holidays: holidaysCollection },
@@ -20,8 +27,38 @@ export function useHolidays(year: number) {
                 .orderBy(
                     ({ holiday_dates }) => holiday_dates.holiday_date,
                     "asc",
-                ),
+                ).select(({ holidays, holiday_dates }) => {
+                    return {
+                        id: holiday_dates.id,
+                        holiday_id: holiday_dates.holiday_id,
+                        holiday_date: holiday_dates.holiday_date,
+                        name: holidays.name,
+                    };
+                }),
         [year],
     );
-    return { query, collection: holidaysCollection };
+
+    const missingHolidaysByYear = useLiveQuery(
+        (q) =>
+            q.from({ holidays: holidaysCollection }).leftJoin(
+                { holiday_dates: holidayDatesByYear.collection },
+                ({ holidays, holiday_dates }) =>
+                    eq(holiday_dates.holiday_id, holidays.id),
+            ).where(({ holiday_dates }) => isUndefined(holiday_dates)).select(
+                ({ holidays }) => {
+                    return {
+                        id: holidays.id,
+                        name: holidays.name,
+                    };
+                },
+            ),
+        [year],
+    );
+
+    return {
+        holidayDatesByYear,
+        missingHolidaysByYear,
+        holidays: holidaysCollection,
+        holiday_dates: holidayDatesCollection,
+    };
 }
