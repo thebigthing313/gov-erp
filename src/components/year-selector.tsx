776f2ee9ed3c4a11c299104ce21c7fb3207c4b30
thebigthing313 <adriankabigting@gmail.com>
2025-10-22 09:39:33 -0400

    feat: implement YearSelector component with increment and decrement functionality

--- src/components/year-selector.tsx
+++ src/components/year-selector.tsx
@@ -1,14 +1,53 @@
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { ButtonGroup } from './ui/button-group'
 import { Button } from './ui/button'
+import { Input } from './ui/input'
+import { HTMLAttributes, Ref } from 'react'
 
-export function YearSelector() {
+interface YearSelectorProps
+  extends Omit<HTMLAttributes<HTMLInputElement>, 'onChange' | 'value'> {
+  value: number
+  className?: string
+  onChange: (year: number) => void
+  ref?: Ref<HTMLInputElement>
+}
+
+export function YearSelector({
+  value,
+  onChange,
+  ref,
+  className,
+  ...props
+}: YearSelectorProps) {
+  const handleDecrement = () => {
+    onChange(value - 1)
+  }
+
+  const handleIncrement = () => {
+    onChange(value + 1)
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = Number(e.target.value)
+    if (!isNaN(newValue)) {
+      onChange(newValue)
+    }
+  }
+
   return (
-    <ButtonGroup>
-      <Button>
+    <ButtonGroup className={className}>
+      <Button onClick={handleDecrement}>
         <ChevronLeft />
       </Button>
-      <Button>
+      <Input
+        ref={ref}
+        {...props}
+        value={value}
+        onChange={handleInputChange}
+        type="text"
+        className="text-center"
+      />
+      <Button onClick={handleIncrement}>
         <ChevronRight />
       </Button>
     </ButtonGroup>
