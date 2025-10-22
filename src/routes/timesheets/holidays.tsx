f74151cdecf2551adf7567af629ac5ce5825b87a
thebigthing313 <adriankabigting@gmail.com>
2025-10-22 09:40:33 -0400

    feat: implement holiday schedule display with YearSelector and HolidayCard components

--- src/routes/timesheets/holidays.tsx
+++ src/routes/timesheets/holidays.tsx
@@ -1,5 +1,8 @@
+import { HolidayCard } from '@/components/cards/holiday-card'
+import { Typography } from '@/components/typography'
 import { YearSelector } from '@/components/year-selector'
 import { useHolidays } from '@/db/hooks/use-holidays'
+import { formatDate } from '@/lib/date-fns'
 import { createFileRoute } from '@tanstack/react-router'
 import { useState } from 'react'
 
@@ -9,7 +12,38 @@ export const Route = createFileRoute('/timesheets/holidays')({
 
 function RouteComponent() {
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear())
-  const { query } = useHolidays(currentYear)
 
-  return <YearSelector />
+  const { query } = useHolidays(currentYear)
+  const holidays = query.data
+
+  return (
+    <div className="max-w-3xl flex flex-col gap-2 items-center">
+      <Typography tag="h2">Holiday Schedule</Typography>
+      <YearSelector
+        value={currentYear}
+        onChange={(year) => setCurrentYear(year)}
+      />
+      <div className="flex flex-row flex-wrap gap-0 justify-center">
+        {holidays.map((holiday, index) => {
+          const { name } = holiday.holidays
+          const { id, holiday_date } = holiday.holiday_dates
+          const formattedDate = formatDate(holiday_date, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+            weekday: 'long',
+          })
+          return (
+            <HolidayCard
+              index={index + 1}
+              key={id}
+              title={name}
+              description={formattedDate}
+              className="min-w-xs not-odd:bg-muted/50"
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
 }
