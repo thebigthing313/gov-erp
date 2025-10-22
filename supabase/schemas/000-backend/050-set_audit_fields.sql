6a761bfc0272565468b7c66009245df4433c691a
thebigthing313 <adriankabigting@gmail.com>
2025-10-22 11:04:41 -0400

    feat: add set_audit_fields trigger function for tracking record creation and modification

---
+++ supabase/schemas/000-backend/050-set_audit_fields.sql
@@ -0,0 +1,25 @@
+create or replace function public.set_audit_fields () returns trigger language plpgsql
+set
+    search_path = '' security invoker as $$
+declare
+    user_id uuid;
+begin
+
+    user_id := auth.uid();
+
+    if TG_OP = 'INSERT' then
+        if user_id is not null then
+            new.created_by = user_id;
+        end if;
+    end if;
+
+    if TG_OP = 'UPDATE' then
+        new.modified_at = now();
+        if user_id is not null then
+            new.modified_by = user_id;
+        end if;
+    end if;
+
+    return new;
+end;
+$$;
