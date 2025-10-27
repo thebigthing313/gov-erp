b0fae1f98b629af70215c653e75e99e172a76565
thebigthing313 <adriankabigting@gmail.com>
2025-10-27 17:10:37 -0400

    feat: include collection parameter in onDelete handler for DBWholeCollectionOptions

--- src/db/whole-collections.ts
+++ src/db/whole-collections.ts
@@ -26,5 +26,6 @@ export const DBWholeCollectionOptions = <T extends Table>(
         collectionOnInsert(table, transaction, collection),
     onUpdate: ({ transaction, collection }) =>
         collectionOnUpdate(table, transaction, collection),
-    onDelete: ({ transaction }) => collectionOnDelete(table, transaction),
+    onDelete: ({ transaction, collection }) =>
+        collectionOnDelete(table, transaction, collection),
 });
