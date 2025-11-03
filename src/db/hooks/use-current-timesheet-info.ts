6afa8d048f1e6e1efd940d3e4e773fa85069025f
thebigthing313 <adriankabigting@gmail.com>
2025-11-03 17:20:23 -0500

    feat: add useCurrentTimesheetInfo hook to retrieve current timesheet and pay period data

---
+++ src/db/hooks/use-current-timesheet-info.ts
@@ -0,0 +1,50 @@
+import { and, eq, gte, lte, useLiveQuery } from "@tanstack/react-db";
+import { usePayPeriods } from "./use-pay-periods";
+import { useTimesheets } from "./use-timesheets";
+
+export function useCurrentTimesheetInfo() {
+    const now = new Date();
+
+    const year = now.getUTCFullYear();
+    const month = now.getUTCMonth();
+    const date = now.getUTCDate();
+
+    const todayMidnightUTC = new Date(Date.UTC(year, month, date));
+
+    const { collection: timesheets_by_year } = useTimesheets(year);
+    const { collection: pay_periods_by_year } = usePayPeriods(year);
+
+    const { data: current_timesheet } = useLiveQuery(
+        (q) =>
+            q.from({ timesheet: timesheets_by_year }).where(({ timesheet }) =>
+                eq(timesheet.timesheet_date, todayMidnightUTC)
+            ).select(({ timesheet }) => {
+                return {
+                    timesheet_date: timesheet.timesheet_date,
+                };
+            }).findOne(),
+        [timesheets_by_year],
+    );
+
+    const { data: current_pay_period } = useLiveQuery(
+        (q) =>
+            q.from({ pay_periods: pay_periods_by_year }).where((
+                { pay_periods },
+            ) => and(
+                lte(pay_periods.begin_date, todayMidnightUTC),
+                gte(pay_periods.end_date, todayMidnightUTC),
+            )).select(({ pay_periods }) => {
+                return {
+                    pay_period_number: pay_periods.pay_period_number,
+                    payroll_year: pay_periods.payroll_year,
+                };
+            }).findOne(),
+        [pay_periods_by_year],
+    );
+
+    return {
+        current_payroll_year: current_pay_period?.payroll_year,
+        current_pay_period: current_pay_period?.pay_period_number,
+        current_timesheet: current_timesheet?.timesheet_date,
+    };
+}
