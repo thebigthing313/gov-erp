150b387dc34dde64bb618bb8663f1e2df7d732de
thebigthing313 <adriankabigting@gmail.com>
2025-11-03 18:16:16 -0500

    refactor: restructure useTimesheets hook to improve query and data handling

--- src/db/hooks/use-timesheets.ts
+++ src/db/hooks/use-timesheets.ts
@@ -1,13 +1,28 @@
-import { useLiveQuery } from "@tanstack/react-db";
-import { getTimesheetsCollection } from "../factories/timesheets";
+import { eq, useLiveQuery } from "@tanstack/react-db";
+import { timesheets } from "../collections/timesheets";
+import { pay_periods } from "../collections/pay-periods";
 
 export function useTimesheets(year: number) {
-    const collection = getTimesheetsCollection(year);
     const query = useLiveQuery(
         (q) =>
-            q.from({ timesheets: collection }).orderBy(
-                ({ timesheets }) => timesheets.timesheet_date,
-            ),
+            q.from({ timesheet: timesheets(year) }).innerJoin({
+                pay_period: pay_periods,
+            }, ({ timesheet, pay_period }) =>
+                eq(
+                    timesheet.pay_period_id,
+                    pay_period.id,
+                )).orderBy(
+                    ({ timesheet }) => timesheet.timesheet_date,
+                ).select(({ timesheet, pay_period }) => {
+                    return {
+                        ...timesheet,
+                        pay_period_number: pay_period.pay_period_number,
+                        payroll_year: pay_period.payroll_year,
+                    };
+                }),
+        [year],
     );
-    return { timesheets_by_year: query, timesheets: collection };
+
+    const { data, collection, isLoading, isError } = query;
+    return { data, collection, isLoading, isError };
 }
