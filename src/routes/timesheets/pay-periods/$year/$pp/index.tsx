25591bc5e4553d85120490d4c63cf7eeb80ade87
thebigthing313 <adriankabigting@gmail.com>
2025-11-07 09:53:54 -0500

    refactor: enhance pay period display with badges and buttons for better user interaction

--- src/routes/timesheets/pay-periods/$year/$pp/index.tsx
+++ src/routes/timesheets/pay-periods/$year/$pp/index.tsx
@@ -1,27 +1,39 @@
-import { Label } from '@/components/ui/label'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemTitle,
+} from '@/components/ui/item'
+import { Spinner } from '@/components/ui/spinner'
+import { timesheets } from '@/db/collections/timesheets'
+import { useIsHoliday } from '@/db/hooks/use-is-holiday'
 import { usePayPeriod } from '@/db/hooks/use-pay-period'
-import { useTimesheets } from '@/db/hooks/use-timesheets'
+import { useTimesheet } from '@/db/hooks/use-timesheet'
+import { ZodTimesheetsRowType } from '@/db/schemas/timesheets'
 import { formatDate } from '@/lib/date-fns'
-import { eq, useLiveSuspenseQuery } from '@tanstack/react-db'
-import { createFileRoute } from '@tanstack/react-router'
-import { useMemo } from 'react'
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router'
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { toast } from 'sonner'
 
 export const Route = createFileRoute('/timesheets/pay-periods/$year/$pp/')({
-  component: RouteComponent,
+  component: () => (
+    <ErrorBoundary fallback={<div>Error loading data</div>}>
+      <Suspense fallback={<Spinner />}>
+        <RouteComponent />
+      </Suspense>
+    </ErrorBoundary>
+  ),
 })
 
 function RouteComponent() {
   const { year, pp } = Route.useParams()
   const { data: pay_period } = usePayPeriod(year, pp)
   type PayPeriod = typeof pay_period
-
-  const { collection: timesheets_by_year } = useTimesheets(year)
-  const { data: timesheets_in_pay_period } = useLiveSuspenseQuery((q) =>
-    q
-      .from({ timesheet: timesheets_by_year })
-      .where(({ timesheet }) => eq(timesheet.pay_period_number, pp))
-      .orderBy(({ timesheet }) => timesheet.timesheet_date),
-  )
 
   function getDates(pay_period: PayPeriod | null | undefined): Date[] {
     if (!pay_period) return []
@@ -41,23 +53,99 @@ function RouteComponent() {
   const dates = getDates(pay_period)
 
   return (
-    <div className="grid grid-cols-7 gap-0">
+    <ItemGroup className="grid grid-rows-7 grid-flow-col w-fit">
       {dates.map((date) => {
         return (
-          <div key={date.toString()} className="flex flex-col p-2">
-            <Label>
-              {formatDate(date, {
-                weekday: 'long',
-                month: 'numeric',
-                day: 'numeric',
-                year: 'numeric',
-              })}
-            </Label>
-            {/* badge to indicate if date is a weekend or holiday */}
-            {/* button to either open existing timesheet or create new one */}
-          </div>
+          <Item className="w-sm" key={date.toISOString()} variant="outline">
+            <ItemContent>
+              <ItemTitle>
+                {formatDate(date, {
+                  weekday: 'long',
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })}
+              </ItemTitle>
+              <ItemDescription>
+                <TimesheetHolidayBadge date={date} />
+                <TimesheetWeekendBadge date={date} />
+              </ItemDescription>
+            </ItemContent>
+            <ItemActions>
+              <TimesheetButton date={date} />
+            </ItemActions>
+          </Item>
         )
       })}
-    </div>
+    </ItemGroup>
   )
+}
+
+function TimesheetWeekendBadge({ date }: { date: Date }) {
+  const dayOfWeek = date.getUTCDay()
+  if (dayOfWeek === 0 || dayOfWeek === 6)
+    return <Badge variant="secondary">Weekend</Badge>
+  else return null
+}
+
+function TimesheetHolidayBadge({ date }: { date: Date }) {
+  const { holiday } = useIsHoliday(date)
+  if (holiday) return <Badge variant="default">{holiday}</Badge>
+  else return null
+}
+
+function TimesheetButton({ date }: { date: Date }) {
+  const { year, pp } = Route.useParams()
+  const { data: pay_period } = usePayPeriod(year, pp)
+  const { data: timesheet } = useTimesheet(date)
+  const navigate = useNavigate()
+  const isoDateString = date.toISOString().substring(0, 10)
+
+  function handleCreate() {
+    const timesheets_by_year = timesheets(year)
+
+    if (!pay_period) {
+      toast.error('Unable to retrieve pay period ID.')
+      return
+    }
+
+    const row: ZodTimesheetsRowType = {
+      id: crypto.randomUUID(),
+      pay_period_id: pay_period.id,
+      timesheet_date: date,
+      notes: null,
+    }
+
+    timesheets_by_year.insert(row)
+    navigate({
+      from: '/timesheets/pay-periods/$year/$pp/',
+      to: './$date',
+      params: { date: isoDateString },
+    })
+  }
+
+  if (timesheet) {
+    return (
+      <Button variant="default" asChild>
+        <Link
+          from="/timesheets/pay-periods/$year/$pp/"
+          to="./$date"
+          params={{ date: isoDateString }}
+        >
+          Open
+        </Link>
+      </Button>
+    )
+  } else {
+    return (
+      <Button
+        variant="secondary"
+        onClick={() => {
+          handleCreate()
+        }}
+      >
+        Create
+      </Button>
+    )
+  }
 }
