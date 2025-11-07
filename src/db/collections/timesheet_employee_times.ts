068d750c2435cf5f26fc943f5b447f220e521078
thebigthing313 <adriankabigting@gmail.com>
2025-11-07 09:53:11 -0500

    refactor: update timesheet_employee_times to use date as key and streamline data fetching

--- src/db/collections/timesheet_employee_times.ts
+++ src/db/collections/timesheet_employee_times.ts
@@ -1,75 +1,72 @@
-import { Collection } from "@tanstack/react-db";
-import { Table } from "../data-types";
-import { supabase } from "../client";
-import { serializeKey } from "../utils";
+import { Collection } from '@tanstack/react-db'
+import { Table } from '../data-types'
+import { supabase } from '../client'
+import { serializeKey } from '../utils'
 import {
-    ZodTimesheetEmployeeTimesInsertToDb,
-    ZodTimesheetEmployeeTimesInsertType,
-    ZodTimesheetEmployeeTimesRow,
-    ZodTimesheetEmployeeTimesRowType,
-    ZodTimesheetEmployeeTimesUpdateType,
-} from "../schemas/timesheet_employee_times";
-import { createParameterizedSupabaseCollectionFactory } from "./collection-factory";
-import { ZodTimesheetEmployeesUpdateToDb } from "../schemas/timesheet_employees";
+  ZodTimesheetEmployeeTimesInsertToDb,
+  ZodTimesheetEmployeeTimesInsertType,
+  ZodTimesheetEmployeeTimesRow,
+  ZodTimesheetEmployeeTimesRowType,
+  ZodTimesheetEmployeeTimesUpdateType,
+} from '../schemas/timesheet_employee_times'
+import { createParameterizedSupabaseCollectionFactory } from './collection-factory'
+import { ZodTimesheetEmployeesUpdateToDb } from '../schemas/timesheet_employees'
 
-type MapKey = { year: number; employee_id: string };
+type MapKey = { date: string }
 
 const cache = new Map<
-    string,
-    Collection<ZodTimesheetEmployeeTimesRowType, string | number>
->();
+  string,
+  Collection<ZodTimesheetEmployeeTimesRowType, string | number>
+>()
 
-const table: Table = "timesheet_employee_times";
+const table: Table = 'timesheet_employee_times'
 
 const timesheetEmployeeTimesCollectionFactory =
-    createParameterizedSupabaseCollectionFactory<
-        [year: number, employee_id: string],
-        ZodTimesheetEmployeeTimesRowType,
-        ZodTimesheetEmployeeTimesInsertType,
-        ZodTimesheetEmployeeTimesUpdateType
-    >(
-        table,
-        {
-            rowSchema: ZodTimesheetEmployeeTimesRow,
-            insertSchema: ZodTimesheetEmployeeTimesInsertToDb,
-            updateSchema: ZodTimesheetEmployeesUpdateToDb,
-        },
-        async (year, employee_id) => {
-            const { data, error } = await supabase.from(table).select(
-                "*, timesheet_employees(timesheets(pay_periods(payroll_year)))",
-            ).eq(
-                "pay_periods.payroll_year",
-                year,
-            ).eq("employee_id", employee_id);
-            if (error) throw error;
-            const strippedData = data.map((item) => {
-                const { timesheet_employees, ...rest } = item;
-                return rest;
-            });
+  createParameterizedSupabaseCollectionFactory<
+    [date: string],
+    ZodTimesheetEmployeeTimesRowType,
+    ZodTimesheetEmployeeTimesInsertType,
+    ZodTimesheetEmployeeTimesUpdateType
+  >(
+    table,
+    {
+      rowSchema: ZodTimesheetEmployeeTimesRow,
+      insertSchema: ZodTimesheetEmployeeTimesInsertToDb,
+      updateSchema: ZodTimesheetEmployeesUpdateToDb,
+    },
+    async (date) => {
+      const { data, error } = await supabase
+        .from(table)
+        .select(
+          '*, timesheet_employees!inner(timesheets!inner(timesheet_date))',
+        )
+        .eq('timesheet_employees.timesheets.timesheet_date', date)
+      if (error) throw error
+      const strippedData = data.map((item) => {
+        const { timesheet_employees, ...rest } = item
+        return rest
+      })
 
-            const parsedData = strippedData.map((item) =>
-                ZodTimesheetEmployeeTimesRow.parse(item)
-            );
+      const parsedData = strippedData.map((item) =>
+        ZodTimesheetEmployeeTimesRow.parse(item),
+      )
 
-            return parsedData as Array<ZodTimesheetEmployeeTimesRowType>;
-        },
-        { staleTime: 1000 * 60 * 30 },
-    );
+      return parsedData as Array<ZodTimesheetEmployeeTimesRowType>
+    },
+    { staleTime: 1000 * 60 * 30 },
+  )
 
-export const timesheet_employees = ({ year, employee_id }: MapKey) => {
-    const key = serializeKey({ year, employee_id });
-    let collection = cache.get(key);
+export const timesheet_employee_times = ({ date }: MapKey) => {
+  const key = serializeKey({ date })
+  let collection = cache.get(key)
 
-    if (!collection) {
-        collection = timesheetEmployeeTimesCollectionFactory(year, employee_id);
+  if (!collection) {
+    collection = timesheetEmployeeTimesCollectionFactory(date)
 
-        collection.on("status:change", ({ status }) => {
-            if (
-                status ===
-                    "cleaned-up"
-            ) cache.delete(key);
-        });
-        cache.set(key, collection);
-    }
-    return collection;
-};
+    collection.on('status:change', ({ status }) => {
+      if (status === 'cleaned-up') cache.delete(key)
+    })
+    cache.set(key, collection)
+  }
+  return collection
+}
