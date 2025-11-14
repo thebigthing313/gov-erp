48b7d560ba00bb32db3f25dab4f843e90735aa72
thebigthing313 <adriankabigting@gmail.com>
2025-11-14 16:30:45 -0500

    fix: refactor usePayPeriod hook to simplify query and remove dependency on usePayPeriods

--- src/db/hooks/use-pay-period.ts
+++ src/db/hooks/use-pay-period.ts
@@ -1,13 +1,29 @@
-import { eq, useLiveSuspenseQuery } from "@tanstack/react-db";
-import { usePayPeriods } from "./use-pay-periods";
+import { eq, useLiveSuspenseQuery } from '@tanstack/react-db'
+import { pay_periods } from '../collections/pay-periods'
 
 export function usePayPeriod(year: number, pp: number) {
-    const { collection: pay_periods_by_year } = usePayPeriods(year);
-    return useLiveSuspenseQuery(
-        (q) =>
-            q.from({ pay_period: pay_periods_by_year }).where((
-                { pay_period },
-            ) => eq(pay_period.pay_period_number, pp)).findOne(),
-        [year, pp],
-    );
+  const query = useLiveSuspenseQuery(
+    (q) =>
+      q
+        .from({ pay_period: pay_periods })
+        .where(({ pay_period }) => eq(pay_period.pay_period_number, pp))
+        .where(({ pay_period }) => eq(pay_period.payroll_year, year))
+        .findOne(),
+    [year, pp],
+  )
+
+  const { data, ...rest } = query
+  if (!data) throw new Error('Pay period not found.')
+  return { data, ...rest }
+}
+
+export function getDatesArray(startDate: Date, endDate: Date): Date[] {
+  const result: Date[] = []
+
+  while (startDate <= endDate) {
+    result.push(new Date(startDate))
+    startDate.setUTCDate(startDate.getUTCDate() + 1)
+  }
+
+  return result
 }
