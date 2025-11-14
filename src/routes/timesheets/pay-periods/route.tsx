78c6e3e241efd9a0a5161b8d41a6fb199b3f7d00
thebigthing313 <adriankabigting@gmail.com>
2025-11-14 16:29:48 -0500

    fix: remove unused GoToCurrentButtons component from pay periods route

--- src/routes/timesheets/pay-periods/route.tsx
+++ src/routes/timesheets/pay-periods/route.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
-import { GoToCurrentButtons } from '../-components/go-to-current-buttons'
 
 export const Route = createFileRoute('/timesheets/pay-periods')({
   component: RouteComponent,
@@ -11,8 +10,6 @@ export const Route = createFileRoute('/timesheets/pay-periods')({
 function RouteComponent() {
   return (
     <div className="flex flex-col gap-2">
-      {/* <GoToCurrentButtons /> */}
-
       <Outlet />
     </div>
   )
